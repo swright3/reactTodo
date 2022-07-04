3daf5b1f5731388fa4565d6e8de0ad5bdee90f5b
sam <samwrighthwk@gmail.com>
2022-07-04 20:13:26 +0200

    Added unique ids and save todos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import TodoList from './TodoList.js';
+import uuid4 from 'uuid/v4'
+
+const LOCAL_STORAGE_KEY = todoApp.todos
 
 function App() {
   //Sets the initial state (no todos) and a function to change the state
-  const [todos, setTodos] = useState([{id: 1, name: "task 1", complete: true}])
+  const [todos, setTodos] = useState([])
+  //Allows referencing of html elements
+  const todoNameRef = useRef()
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') {return}
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: uuidv4(), name: name, completed: false}]
+    })
+    todoNameRef.current.value = null
+  }
+
   //Fragment containing several elements. Only one thing can be returned by a function
   return (
     <> 
       <TodoList todos={todos}/>
-      <input type='text'/>
-      <button>Add Todo</button>
+      <input ref={todoNameRef} type='text'/>
+      <button onClick={handleAddTodo}>Add Todo</button>
       <button>Clear completed todos</button>
       <div>0 left todo</div>
     </>
