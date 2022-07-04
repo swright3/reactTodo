1ef78e592c4d95828444c1425b728003856687ec
sam <samwrighthwk@gmail.com>
2022-07-04 18:19:29 +0200

    Added state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoList from './TodoList.js';
 
 function App() {
+  //Sets the initial state (no todos) and a function to change the state
+  const [todos, setTodos] = useState(['task 1','task 2'])
+  //Fragment containing several elements. Only one thing can be returned by a function
   return (
-    <>
-      <TodoList />
+    <> 
+      <TodoList todos={todos}/>
       <input type='text'/>
       <button>Add Todo</button>
       <button>Clear completed todos</button>
